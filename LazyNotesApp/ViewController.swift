e9348c391c5a2004292e846036076ea9929dfb4a
Polina Reshetilo <polina.reshetilo@gmail.com>
2022-04-14 16:44:20 +0300

    Initial Commit

---
+++ LazyNotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LazyNotesApp
+//
+//  Created by Polina on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
